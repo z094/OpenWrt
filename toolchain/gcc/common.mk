e784a2cd87634f5db9a67acad7e2cca648596d4c
DevriesL <therkduan@gmail.com>
2020-06-05 00:25:44 +0800

    toolchain: remove --enable-target-optspace
    Signed-off-by: DevriesL <therkduan@gmail.com>

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -98,7 +98,6 @@ GCC_CONFIGURE:= \
 		--host=$(GNU_HOST_NAME) \
 		--target=$(REAL_GNU_TARGET_NAME) \
 		--with-gnu-ld \
-		--enable-target-optspace \
 		--disable-libgomp \
 		--disable-libmudflap \
 		--disable-multilib \
